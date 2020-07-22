d8dd04f3064d7199d7f65802bfbad00168104125
Bill Karvelas <vaskarvelas@gmail.com>
2020-07-22 11:59:00 +0300

    Wire Up Login Form to API
    Connects the LogIn button to the authentication API endpoint (/Token). Gets back the bearer token or an exception if failed.

---
+++ TRMDesktopUI/Helpers/APIHelper.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using TRMDesktopUI.Models;
+
+namespace TRMDesktopUI.Helpers
+{
+    public class APIHelper : IAPIHelper
+    {
+        private HttpClient apiClient;
+
+        public APIHelper()
+        {
+            InitializeClient();
+        }
+
+        private void InitializeClient()
+        {
+            // grab the value from App.config using the key = "api"
+            string api = ConfigurationManager.AppSettings["api"];
+
+            // initiallize the ApiClient
+            apiClient = new HttpClient();
+
+            // set the BaseAddress
+            apiClient.BaseAddress = new Uri(api);
+
+            apiClient.DefaultRequestHeaders.Accept.Clear();
+
+            // we want to work with json not xml
+            apiClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        }
+
+        public async Task<AuthenticatedUser> Authenticate(string username, string password)
+        {
+            var data = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "password"),
+                new KeyValuePair<string, string>("username", username),
+                new KeyValuePair<string, string>("password", password)
+            });
+
+            // communicates with the api to authenticate the User
+            using (HttpResponseMessage response = await apiClient.PostAsync("/Token", data))
+            {
+                if (response.IsSuccessStatusCode)
+                {
+                    var result = await response.Content.ReadAsAsync<AuthenticatedUser>();
+                    return result;
+                }
+                else
+                {
+                    throw new Exception(response.ReasonPhrase);
+                }
+            }
+        }
+    }
+}
---
+++ TRMDesktopUI/ViewModels/LoginViewModel.cs
@@ -0,0 +1,72 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TRMDesktopUI.Helpers;
+
+namespace TRMDesktopUI.ViewModels
+{
+    public class LoginViewModel : Screen
+    {
+        private string _userName;
+        private string _password;
+        private IAPIHelper _apiHelper;
+
+        public LoginViewModel(IAPIHelper apiHelper)
+        {
+            _apiHelper = apiHelper;
+        }
+
+        /// <summary>
+        /// gets the UserName from the ui user control - sets the UserName 
+        /// </summary>
+        public string UserName
+        {
+            get { return _userName; }
+            set
+            {
+                _userName = value;
+                NotifyOfPropertyChange(() => UserName);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        /// <summary>
+        /// gets the Password from the ui user control - sets the Password 
+        /// </summary>
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                NotifyOfPropertyChange(() => Password);
+                NotifyOfPropertyChange(() => CanLogIn);
+            }
+        }
+
+        // Enable Log In button only if UserName and Password fields are not empty
+        public bool CanLogIn => (!string.IsNullOrWhiteSpace(UserName) && !string.IsNullOrWhiteSpace(Password));
+
+        /// <summary>
+        /// authenticates the User
+        /// </summary>
+        /// <returns> Access_Token, UserName </returns>
+        public async Task LogIn()
+        {
+            try
+            {
+                var result = await _apiHelper.Authenticate(UserName, Password);
+
+                Console.WriteLine($"Access_Token: {result.Access_Token}");
+                Console.WriteLine($"UserName: {result.UserName}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
