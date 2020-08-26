99667b8babca464add54fe4ec3b311117b889bb0
Bill Karvelas <vaskarvelas@gmail.com>
2020-08-26 10:24:49 +0300

    Get Add To Cart Working
    Adds the functionality to add items to the cart, get the subtotal, and update quantities.

--- TRMDesktopUI/ViewModels/SalesViewModel.cs
+++ TRMDesktopUI/ViewModels/SalesViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using Caliburn.Micro;
 using System.ComponentModel;
+using System.Linq;
 using System.Threading.Tasks;
 using TRMDesktopUI.Library.Api;
 using TRMDesktopUI.Library.Models;
@@ -9,8 +10,6 @@ namespace TRMDesktopUI.ViewModels
     public class SalesViewModel : Screen
     {
         private IProductEndpoint _productEndpoint;
-        private BindingList<ProductModel> _products;
-        private BindingList<ProductModel> _cart;
 
         public SalesViewModel(IProductEndpoint productEndpoint)
         {
@@ -36,6 +35,8 @@ namespace TRMDesktopUI.ViewModels
             Products = new BindingList<ProductModel>(productList);
         }
 
+        private BindingList<ProductModel> _products;
+
         public BindingList<ProductModel> Products
         {
             get { return _products; }
@@ -46,7 +47,22 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        public BindingList<ProductModel> Cart
+        private ProductModel _selectedProduct;
+
+        public ProductModel SelectedProduct
+        {
+            get { return _selectedProduct; }
+            set
+            {
+                _selectedProduct = value;
+                NotifyOfPropertyChange(() => SelectedProduct);
+                NotifyOfPropertyChange(() => CanAddToCart);
+            }
+        }
+
+        private BindingList<CartItemModel> _cart = new BindingList<CartItemModel>();
+
+        public BindingList<CartItemModel> Cart
         {
             get { return _cart; }
             set
@@ -56,7 +72,7 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        private int _itemQuantity;
+        private int _itemQuantity = 1;
 
         public int ItemQuantity
         {
@@ -65,15 +81,21 @@ namespace TRMDesktopUI.ViewModels
             {
                 _itemQuantity = value;
                 NotifyOfPropertyChange(() => ItemQuantity);
+                NotifyOfPropertyChange(() => CanAddToCart);
             }
         }
 
         public string SubTotal
         {
-            // TODO: Replace with calculation
             get
             {
-                return "$0.00";
+                decimal subTotal = 0;
+
+                foreach (var item in Cart)
+                {
+                    subTotal += (item.QuantityInCart * item.Product.RetailPrice);
+                }
+                return subTotal.ToString("C");
             }
         }
 
@@ -95,14 +117,34 @@ namespace TRMDesktopUI.ViewModels
             }
         }
 
-        //public bool CanAddToCart => (
-        //    // TODO: Make sure something is selected
-        //    // TODO: Make sure there is an item quantity
-
-        //    );
+        // TODO: Make sure something is selected = OK!
+        // TODO: Make sure there is an item quantity = OK!
+        public bool CanAddToCart => (ItemQuantity > 0 && SelectedProduct?.QuantityInStock >= ItemQuantity);
 
         public void AddToCart()
         {
+            CartItemModel existingItem = Cart.FirstOrDefault(x => x.Product == SelectedProduct);
+
+            if (existingItem != null)
+            {
+                Cart.Remove(existingItem);
+                existingItem.QuantityInCart += ItemQuantity;
+                Cart.Add(existingItem);
+            }
+            else
+            {
+                CartItemModel item = new CartItemModel
+                {
+                    Product = SelectedProduct,
+                    QuantityInCart = ItemQuantity
+                };
+
+                Cart.Add(item);
+            }
+
+            SelectedProduct.QuantityInStock -= ItemQuantity;
+            ItemQuantity = 1;
+            NotifyOfPropertyChange(() => SubTotal);
 
         }
 
@@ -112,7 +154,7 @@ namespace TRMDesktopUI.ViewModels
 
         public void RemoveFromCart()
         {
-
+            NotifyOfPropertyChange(() => SubTotal);
         }
 
         //public bool CanCheckOut => (
